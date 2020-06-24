833049ceeb91d7468c4ff9a686c8ae2e59ffbad9
Ruben Sanchez Garcia <rubensanchezgarc@gmail.com>
2020-06-25 00:59:51 +0200

    typo fixed

--- condaDeepEMHancer/build_conda_pkg.sh
+++ condaDeepEMHancer/build_conda_pkg.sh
@@ -2,10 +2,10 @@
 anaconda_path=`which python | rev | cut -f 3- -d"/" | rev` &&
 rm $anaconda_path/conda-bld/linux-64/*
 
-conda-build . -c anaconda -c conda-forge --python=3.6 # &&
+conda-build . -c anaconda -c conda-forge --python=3.6 &&
 
 #conda install --use-local deepEMhancer -c anaconda -c conda-forge
 #conda install   deepEMhancer -c file://${HOME}/app/anaconda3/envs/deepEMhancer_env/conda-bld/ -c anaconda -c conda-forge
 
-echo rsanchez1369 | anaconda upload $anaconda_path/conda-bld/linux-64/deepEMhancer-*.bz2  # && conda build purge
+echo rsanchez1369 | anaconda upload $anaconda_path/conda-bld/linux-64/deepemhancer-*.bz2  # && conda build purge
 #xxxXxx
