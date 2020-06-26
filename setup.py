dab940987c86a71511ee061f99a6aa932049e76e
Ruben Sanchez Garcia <rubensanchezgarc@gmail.com>
2020-06-26 14:30:27 +0200

    v0.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 import setuptools
 from setuptools import setup
 
-VERSION="0.01"
+VERSION="0.11"
 
 def readme():
   readmePath = os.path.abspath(os.path.join(__file__, "..", "README.md"))
