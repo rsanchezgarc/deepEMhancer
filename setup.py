418e84f6db7dd9247a6dc300b4d9329b472be72e
Ruben Sanchez Garcia <rubensanchezgarc@gmail.com>
2020-09-08 00:11:41 +0200

    change version id

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 import setuptools
 from setuptools import setup
 
-VERSION="0.12"
+VERSION="0.13"
 
 def readme():
   readmePath = os.path.abspath(os.path.join(__file__, "..", "README.md"))
